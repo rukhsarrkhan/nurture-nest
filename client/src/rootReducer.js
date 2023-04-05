55db24621dc3030cd31c354d0fd24fffb91fab2f
Swarajsing Patil <swarajsingpatil@gmail.com>
2023-04-05 15:39:31 -0400

    feat: createJob form works and creates DB entry

--- client/src/rootReducer.js
+++ client/src/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import userReducer from './redux/users/userReducer';
+import jobReducer from './redux/jobs/jobReducer';
 const rootReducer = combineReducers({
-    users: userReducer
+    users: userReducer,
+    jobs: jobReducer
 });
 
 export default rootReducer;
--- server/routes/job.js
+++ server/routes/job.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const data = require("../data");
+const jobCollection = data.job;
 
 router
-    .route("/")
+    .route("/createJob")
     .post(async (req, res) => {
-    });
+        console.log("in post createJob")
+        let { shifts, description, address, specialCare, salary } = req.body;
+        try {
+              const jobCreated = await jobCollection.createJob(shifts, description, address, specialCare, salary);
+              if (!jobCreated) { throw "Couldn't Create job"; }
+              return res.json(jobCreated);
+        } catch (e) { 
+            throw e
+              return res.status(400).json({ error: e }); 
+        }
+  });
 router
     .route('/:jobId')
     .get(async (req, res) => {

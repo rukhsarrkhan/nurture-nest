38627ebd9b74a87a72ed46b7ce4ffab7a0ae4522
Dipankar Bhatia <dbhatia2@stevens.edu>
2023-04-24 19:49:41 -0400

    Refactored Dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,9 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import '../App.css';
-import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from "react-redux";
 import {
   Card,
   CardActionArea,
@@ -13,13 +11,12 @@ import {
   Typography
 } from '@mui/material';
 import { getDashboardAPICall } from '../redux/dashboard/dashboardActions';
+import Navbar from './Navbar';
 
 const Dashboard = ({ getDashboardAPICall, dashboardData }) => {
   console.log(dashboardData, "new dashboard data")
-  const regex = /(<([^>]+)>)/gi;
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  const [data,setData] = useState([])
   let { nannyId } = useParams();
   let card = null;
 
@@ -36,19 +33,10 @@ const Dashboard = ({ getDashboardAPICall, dashboardData }) => {
         console.log(e);
       }
     }
-    if(nannyId != undefined) { 
-      fetchData()}
+    if (nannyId !== undefined) {
+      fetchData()
+    }
   }, [nannyId]);
-
-//   card =
-//         dashboardData &&
-//         dashboardData.data &&
-//         dashboardData.data.map((dashboard) => {
-//             if (dashboard !== null) {
-//                 return buildCard(dashboard);
-//         }
-// });
-
 
   if (loading) {
     return (
@@ -57,96 +45,96 @@ const Dashboard = ({ getDashboardAPICall, dashboardData }) => {
       </div>
     );
   } else if (error) {
-      return (<div>
-              Error here
-      </div>
+    return (<div>
+      Error here
+    </div>
     );
-    }else{
-      console.log(data)
-      return (
-        <Grid container spacing={2}>
-      <Grid item xs={6} key={data.nannyId}>
-        <Card
-          variant='outlined'
-         
-        >
-          <CardActionArea>
-            <Link to={`/meal/${dashboardData.childId}`}>
-              <CardMedia
-                sx={{
-                  height: '100%',
-                  width: '100%'
-                }}
-                component='img'
-                image={ "" }      
-                title='Meal Requirements'
-              />
+  } else {
+    console.log(dashboardData, "data aaya")
+    return (
+      <Grid container spacing={2}>
+        <Grid item xs={6} key={dashboardData && dashboardData.data && dashboardData.data.nannyId}>
+          <Card
+            variant='outlined'
 
-              <CardContent>
-                <Typography
+          >
+            <CardActionArea>
+              <Link to={`/meal/${dashboardData.data.childId}`}>
+                <CardMedia
                   sx={{
-                    borderBottom: '1px solid #1e8678',
-                    fontWeight: 'bold'
+                    height: '100%',
+                    width: '100%'
                   }}
-                  gutterBottom
-                  variant='h6'
-                  component='h2'
-                >
-                </Typography>
-                <Typography variant='body2' color='textSecondary' component='p'>
-                  {dashboardData.data
-                    ? dashboardData.data.mealRequirements[0]
-                    : 'No data to display'}
-                </Typography>
-                <dl>
-                    </dl>
-              </CardContent>
-            </Link>
-          </CardActionArea>
-        </Card>
-      </Grid>
-      <Grid item xs={6} key={dashboardData.data.nannyId}>
-        <Card
-          variant='outlined'
-         
-        >
-          <CardActionArea>
-            <Link to={`/vaccine/${dashboardData.data.childId}`}>
-              <CardMedia
-                sx={{
-                  height: '100%',
-                  width: '100%'
-                }}
-                component='img'
-                image={ "" }      
-                title='Vaccines'
-              />
+                  component='img'
+                  image={""}
+                  title='Meal Requirements'
+                />
 
-              <CardContent>
-                <Typography
+                <CardContent>
+                  <Typography
+                    sx={{
+                      borderBottom: '1px solid #1e8678',
+                      fontWeight: 'bold'
+                    }}
+                    gutterBottom
+                    variant='h6'
+                    component='h2'
+                  >
+                  </Typography>
+                  <Typography variant='body2' color='textSecondary' component='p'>
+                    {dashboardData.data && dashboardData.data.mealRequirements[0]
+                      ? dashboardData.data.mealRequirements[0]
+                      : 'No data to display'}
+                  </Typography>
+                  <dl>
+                  </dl>
+                </CardContent>
+              </Link>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item xs={6} key={dashboardData && dashboardData.data && dashboardData.data.nannyId}>
+          <Card
+            variant='outlined'
+
+          >
+            <CardActionArea>
+              <Link to={`/vaccine/${dashboardData.data.childId}`}>
+                <CardMedia
                   sx={{
-                    borderBottom: '1px solid #1e8678',
-                    fontWeight: 'bold'
+                    height: '100%',
+                    width: '100%'
                   }}
-                  gutterBottom
-                  variant='h6'
-                  component='h2'
-                >
-                  {data.name}
-                </Typography>
-                <Typography variant='body2' color='textSecondary' component='p'>
-                  {dashboardData.data.vaccines && dashboardData.data.vaccineDetails[0].vaccines.name
-                    ? dashboardData.data.vaccineDetails[0].name
-                    : 'No data to display'}
-                </Typography>
-                <dl>
-                    </dl>
-              </CardContent>
-            </Link>
-          </CardActionArea>
-        </Card>
-      </Grid>
-           <Grid item xs={6} key={data.nannyId}>
+                  component='img'
+                  image={""}
+                  title='Vaccines'
+                />
+
+                <CardContent>
+                  <Typography
+                    sx={{
+                      borderBottom: '1px solid #1e8678',
+                      fontWeight: 'bold'
+                    }}
+                    gutterBottom
+                    variant='h6'
+                    component='h2'
+                  >
+                    {dashboardData.data.vaccineDetails[0].name}
+                  </Typography>
+                  <Typography variant='body2' color='textSecondary' component='p'>
+                    {dashboardData.data.vaccines && dashboardData.data.vaccineDetails[0] && dashboardData.data.vaccineDetails[0].name
+                      ? dashboardData.data.vaccineDetails[0].name
+                      : 'No data to display'}
+                  </Typography>
+                  <dl>
+                  </dl>
+                </CardContent>
+              </Link>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        {/* <Grid item xs={6} key={data.nannyId}>
         <Card
           variant='outlined'
          
@@ -185,27 +173,27 @@ const Dashboard = ({ getDashboardAPICall, dashboardData }) => {
             </Link>
           </CardActionArea>
         </Card>
-      </Grid>
-
+      </Grid> */}
       </Grid>
     );
   };
-    
-  };
 
-  const mapStateToProps = state => {
-    return {
-        dashboardData: state.dashboard
-    };
+};
+
+const mapStateToProps = state => {
+  console.log("state", state)
+  return {
+    dashboardData: state.dashboard
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        getDashboardAPICall: (nannyId) => dispatch(getDashboardAPICall(nannyId)),
-    };
+  return {
+    getDashboardAPICall: (nannyId) => dispatch(getDashboardAPICall(nannyId)),
+  };
 };
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Dashboard);

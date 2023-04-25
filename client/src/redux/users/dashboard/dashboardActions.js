058087c031a06e841fef2791c8c4f1113283e34e
Dipankar Bhatia <dbhatia2@stevens.edu>
2023-04-25 13:47:46 -0400

    Dashboard redux implementation

---
+++ client/src/redux/users/dashboard/dashboardActions.js
@@ -0,0 +1,51 @@
+import {
+    GET_DASHBOARD_SUCCESS,
+    GET_DASHBOARD_FAILURE,
+    SET_DASHBOARD_SUCCESS,
+    SET_DASHBOARD_FAILURE
+} from './dashboardActionTypes'
+import axios from 'axios'
+
+export const getDashboardSuccess = (nanny) => {
+    return {
+      type: GET_DASHBOARD_SUCCESS,
+      payload: nanny,
+    };
+  };
+
+export const getDashboardFailure = (error) => {
+    return {
+      type: GET_DASHBOARD_FAILURE,
+      payload: error,
+    };
+  };
+
+export const dashboardSetSuccess = (nanny) => {
+    return {
+      type: SET_DASHBOARD_SUCCESS,
+      payload: nanny,
+    };
+  };
+
+export const dashboardSetFailure = (error) => {
+    return {
+      type: SET_DASHBOARD_FAILURE,
+      payload: error,
+    };
+  };
+
+  export const getDashboardAPICall = (nannyId) => {
+    console.log(nannyId,"nannyId in a ction call")
+    return async (dispatch) => {
+      try {
+        let resp = await axios.get(`http://localhost:3000/nanny/dashboard/${nannyId}`);
+        console.log(resp.data,"got resp data here:")
+        // set token here
+
+        // sessionStorage.setItem("token", resp.data.token);
+        dispatch(getDashboardSuccess(resp.data));
+      } catch (error) {
+        dispatch(getDashboardFailure(error));
+      }
+    };
+  };
